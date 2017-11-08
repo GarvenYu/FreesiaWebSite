7e6c725c31eb85bb86d09be62e449b270043cb67
ykbboom <1747603726@qq.com>
2017-11-08 22:39:37 +0800

    添加连接吃和select方法

--- web/static/orm/Model.py
+++ web/static/orm/Model.py
@@ -1,5 +1,6 @@
 # -*-coding:utf-8-*-
 import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, aiomysql
 from web.static.orm import ModelMetaclass
 
 
@@ -27,3 +28,36 @@ class Model(dict, metaclass=ModelMetaclass):
                 logging.info("find default value (key:%s) = (val:%s)" % (key, field_value))
                 setattr(self, key, field_value)
         return field_value
+
+
+@asyncio.coroutine
+def create_pool(loop, **kwargs):
+    logging.info("create database connection pool...")
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kwargs.get('host', 'localhost'),
+        port=kwargs.get('port', 3306),
+        user=kwargs['user'],
+        password=kwargs['password'],
+        db=kwargs['db'],
+        charset=kwargs.get('charset', 'utf8'),
+        autocommit=kwargs.get('autocommit', True),
+        maxsize=kwargs.get('maxsize', 10),
+        minsize=kwargs.get('minsize', 1),
+        loop=loop
+    )
+
+
+@asyncio.coroutine
+def select(sql, args, size=None):
+    logging.info("sql->(%s),args->(%s)" % (sql, args))
+    with (yield from __pool) as conn:
+        cursor = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cursor.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cursor.fetchmany(size)
+        else:
+            rs = yield from cursor.fetchall()
+        yield from cursor.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
