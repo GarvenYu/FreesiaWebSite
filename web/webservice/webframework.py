667d4b36a1246c9fc93300e89d28db5e6ed71e13
ykbboom <1747603726@qq.com>
2017-11-15 13:05:55 +0800

    webframework use model inspect judge URL func--1

--- web/webservice/webframework.py
+++ web/webservice/webframework.py
@@ -3,6 +3,9 @@
 
 
 import functools
+import inspect, asyncio
+from aiohttp import web
+from urllib import parse
 
 # 定义GET/POST方法的装饰器，分别添加__method__属性和__route__属性，来标记某个URL处理函数的性质以及路径
 # @get('/path') @post('/path')
@@ -31,3 +34,23 @@ def post(path):
         wrapper.__route__ = path
         return wrapper
     return decorator
+
+
+# 使用inspect模块，来处理映射不同URL的函数的所需参数和request参数的关系
+
+# 收集没有默认值的命名关键字参数
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, value in params.items():
+        if str(value.kind) == 'KEYWORD_ONLY' and value.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+
+def test_func(*,name = 'ykb'):
+    pass
+
+
+a = get_required_kw_args(test_func())
+print(str(a))
