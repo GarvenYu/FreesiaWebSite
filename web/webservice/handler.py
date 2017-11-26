6dccdcbe20952f36b2bb3c707f80adc75a67c2a0
ykbboom <1747603726@qq.com>
2017-11-26 17:02:39 +0800

    add static resource，use uikit build html page

--- web/webservice/handler.py
+++ web/webservice/handler.py
@@ -4,13 +4,12 @@
 
 from web.webservice.webframework import get, post
 from web.webservice.orm.WebSiteModel import User
-import asyncio
 
 
 @get('/main')
 async def index(request):
     users = await User.find_all_user()
     return {
-        '__template__': 'test.html',
+        '__template__': 'blog_list.html',
         'users': users
     }
