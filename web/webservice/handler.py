9b85adebdaf5c5e35e618d91ffab6f0a3ea4260c
ykbboom <1747603726@qq.com>
2017-12-14 22:49:05 +0800

    achieve function of pagination,completed

--- web/webservice/handler.py
+++ web/webservice/handler.py
@@ -23,17 +23,21 @@ async def index():
 
 @get('/blog/blogList')
 async def get_blog_list(*, page='1'):
-    summary = 'catch me if you can'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200),
-        Blog(id='4', name='Learn Python', summary=summary, created_at=time.time() - 10000),
-        Blog(id='5', name='Learn Java', summary=summary, created_at=time.time() - 20000)
-    ]
+    # summary = 'catch me if you can'
+    # blogs = [
+    #     Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+    #     Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+    #     Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200),
+    #     Blog(id='4', name='Learn Python', summary=summary, created_at=time.time() - 10000),
+    #     Blog(id='5', name='Learn Java', summary=summary, created_at=time.time() - 20000)
+    # ]
+    # return {
+    #     '__template__': 'blog_list.html',
+    #     'blogs': blogs
+    # }
     return {
         '__template__': 'blog_list.html',
-        'blogs': blogs
+        'page_index': get_page_index(page)
     }
 
 
